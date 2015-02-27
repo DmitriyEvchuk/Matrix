c1a9a0940a8add324358759db9078a401ccad6cb
DmitriyEvchuk <dmitriy.evchuk@gmail.com>
2015-02-27 23:36:57 +0200

    Add

--- Words/src/Chars.java
+++ Words/src/Chars.java
@@ -3,7 +3,7 @@ abstract public class Chars {
 	static boolean charCompare(char a, char b) {
 
 		if (Character.isLetter(a) && Character.isLetter(b)) {
-
+//32 the difference between the uppercase and loweercase
 			if ((a - b) == 32)
 				return true;
 
